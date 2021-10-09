26a6b9d1b0d6ce49a45113cfe580f674421ab42b
jharveyramos <90462624+jharveyramos@users.noreply.github.com>
2021-10-08 19:54:29 -0500

    primer commit interface usuarios

---
+++ js/menu.js
@@ -0,0 +1,11 @@
+ 
+ /*js para animación de menu*/
+ const btn = document.querySelector("#menu-btn");
+      const menu = document.querySelector("#sidemenu");
+      btn.addEventListener("click", (e) => {
+        menu.classList.toggle("menu-expanded");
+        menu.classList.toggle("menu-collapsed");
+
+        document.querySelector("body").classList.toggle("body-expanded");
+      });
+/******************************************* */
